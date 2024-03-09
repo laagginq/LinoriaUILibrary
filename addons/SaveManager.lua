fd8fea69bbf3ccff4c29952f38ca0fdcd4945acb
xz <95953670+laagginq@users.noreply.github.com>
2024-03-08 20:01:31 -0800

    Update SaveManager.lua

--- addons/SaveManager.lua
+++ addons/SaveManager.lua
@@ -1,7 +1,7 @@
 local httpService = game:GetService('HttpService')
 
 local SaveManager = {} do
-	SaveManager.Folder = 'Alysum'
+	SaveManager.Folder = 'alysum'
 	SaveManager.Ignore = {}
 	SaveManager.Parser = {
 		Toggle = {
